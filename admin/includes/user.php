0ddaa9258866f584236db3f762abedf6839d4d79
Ed Wince <1half.ed@gmail.com>
2020-04-29 21:49:45 -0600

    adding all files initially

---
+++ admin/includes/user.php
@@ -0,0 +1,81 @@
+<?php
+	
+	class User {
+		public $id;
+		public $username;
+		public $password;
+		public $firstname;
+		public $lastname;
+
+		public static function find_all_users() {
+			return self::find_this_query("SELECT * FROM users");
+		}
+
+		public static function find_user_by_id($user_id) {
+			//global $database;
+			//$result_set = $database->query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
+			$the_result_array = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
+			//$found_user = mysqli_fetch_array($the_result_array);
+			/*if(!empty($the_result_array)) {
+				$first_item = array_shift($the_result_array);
+				return $first_item;
+			}else {
+				return false;
+			}*/
+			return !empty($the_result_array) ? array_shift($the_result_array) : false;
+			//return $found_user;
+		}
+
+		public static function find_this_query($sql) {
+			global $database;
+
+			$result_set = $database->query($sql);
+			$the_object_array = array();
+
+			while($row = mysqli_fetch_array($result_set)) {
+				$the_object_array[] = self::instantiation($row);
+			}
+
+			//return $result_set;
+			return $the_object_array;
+		}
+
+		public static function verify_user($username, $password) {
+			global $database;
+
+			$username = $database->escape_string($username);
+			$password = $database->escape_string($password);
+
+			$sql = "SELECT * FROM users WHERE ";
+			$sql .= "username = '{$username}' ";
+			$sql .= "AND password = '{$password}' LIMIT 1";
+
+			$the_result_array = self::find_this_query($sql);
+
+			return !empty($the_result_array) ? array_shift($the_result_array) : false;
+		}
+
+		public static function instantiation($the_record) {
+			$the_object = new self;
+
+			/*$the_object->id        = $found_user['id'];
+			$the_object->username  = $found_user['username'];
+			$the_object->password  = $found_user['password'];
+			$the_object->firstname = $found_user['first_name'];
+			$the_object->lastname  = $found_user['last_name'];*/
+
+			foreach($the_record as $the_attribute => $value) {
+				if($the_object->has_the_attribute($the_attribute)) {
+					$the_object->$the_attribute = $value;
+				}
+			}
+
+			return $the_object;
+		}
+
+		private function has_the_attribute($the_attribute) {
+			$object_properties = get_object_vars($this);
+			return array_key_exists($the_attribute, $object_properties);
+		}
+	}
+?>
